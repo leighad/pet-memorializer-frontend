6921b72ca12564c1a2ca74c2833a35b711d4afde
leighad <cdversiontwo@gmail.com>
2020-09-09 16:32:24 -0700

    create addPet

---
+++ src/actions/addPet.js
@@ -0,0 +1,15 @@
+export const addPet = (formData) => {
+
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/pets', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(formData)
+        })
+        .then(res => res.json())
+        .then(pet => dispatch({type: 'ADD_PET', payload: pet}))
+    }
+}
