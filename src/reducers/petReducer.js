68b67468e1c328e20b85365bf9140a7625fe091f
leighad <cdversiontwo@gmail.com>
2020-09-09 16:33:20 -0700

    set up add pet action

--- src/reducers/petReducer.js
+++ src/reducers/petReducer.js
@@ -3,6 +3,9 @@ export default function petReducer(state = {pets: []}, action) {
     switch (action.type) {
         case 'FETCH_PETS':
             return { pets: action.payload } 
+            
+        case 'ADD_PET':
+            return {...state, pets: [...state.pets, action.payload]}
 
         default:
             return state;
