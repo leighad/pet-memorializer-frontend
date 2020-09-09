c2631b9c3f4c3d6a90b243627923c615eeed8345
leighad <cdversiontwo@gmail.com>
2020-09-08 20:04:10 -0700

    set up Pet Input

---
+++ src/components/PetInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class PetInput extends React.Component {
+
+    render() {
+        return (
+            <div>
+                PetInput
+            </div>
+        )
+    }
+
+}
+
+export default PetInput;
