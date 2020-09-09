c2c3635c4905cc5a137dc095b7cbc160b89bd291
leighad <cdversiontwo@gmail.com>
2020-09-08 23:33:05 -0700

    change store declaration to single line

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import petReducer from './reducers/petReducer'
 
 import './index.css';
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(petReducer, 
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
-);
+const store = createStore(petReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
