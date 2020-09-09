cff10e4d8360a43db2db5a3af65794d4e5038766
leighad <cdversiontwo@gmail.com>
2020-09-08 23:56:39 -0700

    set up fetch pets

--- src/containers/PetsContainer.js
+++ src/containers/PetsContainer.js
@@ -1,18 +1,31 @@
 import React from 'react' 
+import { connect } from 'react-redux'
+
+import { fetchPets } from '../actions/fetchPets'
 import Pets from '../components/Pets'
 import PetInput from '../components/PetInput'
 
 class PetsContainer extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchPets()
+    }
 
     render() {
 
         return (
             <div>
                 <PetInput />
-                <Pets />
+                <Pets pets={this.props.pets}/>
             </div>
         )
     }
 }
 
-export default PetsContainer;
+const mapStateToProps = state => {
+    return {
+        pets: state.pets 
+    }
+}
+
+export default connect(mapStateToProps, { fetchPets })(PetsContainer);
