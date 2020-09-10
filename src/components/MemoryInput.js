4191e6a74ed82253396bd919ee1501a7b2a0cef7
leighad <cdversiontwo@gmail.com>
2020-09-10 16:17:23 -0700

    set up Memory Input

---
+++ src/components/MemoryInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class MemoryInput extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Memory Input Form Here
+            </div>
+        )
+    }
+}
+
+
+export default MemoryInput;
