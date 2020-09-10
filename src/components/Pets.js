58d891dcc5b4f21b94734ceeb70297ba8eaa8c6e
leighad <cdversiontwo@gmail.com>
2020-09-09 17:13:05 -0700

    modify to render PetShow

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import PetShow from './PetShow'
 
 const Pets = (props) => {
 
     return (
         <div>
-            {props.pets.map(pet => <li key={pet.id}>{pet.name} the {pet.kind}</li>)}
+            {/* {props.pets.map(pet => <li key={pet.id}>{pet.name} the {pet.kind}</li>)} */}
+            {props.pets.map(pet => 
+                <div key={pet.id}><PetShow pet={pet}/></div>)}
         </div>
     )
 }
