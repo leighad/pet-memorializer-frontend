a7de78269084ac5fa81213e2ffccb7dcf6d74466
leighad <cdversiontwo@gmail.com>
2020-09-10 16:17:52 -0700

    set up Memories

---
+++ src/components/Memories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+const Memories = (props) => {
+
+    return (
+        <div>
+            List of Memories
+            {/* <h2>`${props.pets[pet.name]}'s memories`</h2>
+            {props.pets.memories.map(pet => 
+                <li key={pet.id}>
+                    <Link to={`/pets/${pet.id}`}>{pet.name}</Link>
+                </li> )} */}
+        </div>
+    )
+}
+
+export default Memories;
