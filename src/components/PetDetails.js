c10f6e6655bbcfd95a0573462908ddc7973516db
leighad <cdversiontwo@gmail.com>
2020-09-11 23:18:24 -0700

    set up Pet Details

---
+++ src/components/PetDetails.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PetDetails = (props) => {
+
+    console.log(props)
+    // let currentPet = 
+
+    return (
+        <div>
+            Pet Details Go Here
+            {/* <h4>`${currentPet.name} the ${currentPet.kind}`</h4> */}
+            
+        </div>
+    )
+}
+
+export default PetDetails;
