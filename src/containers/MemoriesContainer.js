28ac04e7f364add65e754685e3734f56efac31af
leighad <cdversiontwo@gmail.com>
2020-09-12 01:23:56 -0700

    building memory show route in progress

--- src/containers/MemoriesContainer.js
+++ src/containers/MemoriesContainer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import MemoryInput from '../components/MemoryInput'
 import MemoryShow from '../components/MemoryShow'
@@ -10,8 +9,12 @@ class MemoriesContainer extends React.Component {
     render() {
         return (
             <div>
-                <MemoryInput /><br/>
-                <MemoryShow memory={this.props.pet && this.props.pet.memory_id}/>
+            
+                <MemoryInput pet={this.props.pet}/><br/>
+                <Switch>
+                    {/* <MemoryShow memory={this.props.pet && this.props.pet.memory_id}/> */}
+                    <Route path='/pets/`${this.props.id}`/memories/:id' render={(routerProps) => <MemoryShow { ...routerProps } memories={this.props.memories} /> }/>
+                </Switch>
                 <Memories memories={this.props.pet && this.props.pet.memories}/>
             </div>
         )
@@ -25,3 +28,5 @@ export default MemoriesContainer;
 // <Route path='/pets/:id' render={(routerProps) => <PetShow { ...routerProps } pets={this.props.pets} /> }/>
 // <Route path='/pets' render={(routerProps) => <Pets { ...routerProps } pets={this.props.pets} /> }/>
 // </Switch>
+
+{/* <Route path='pets/:id/memories/:id' render={(routerProps) => <MemoryShow { ...routerProps } memories={this.props.memories} /> }/> */}
