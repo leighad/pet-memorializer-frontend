8c50d6f7978173d2749ece7839f028cb95234aa3
leighad <cdversiontwo@gmail.com>
2020-09-11 23:21:56 -0700

    add connect and Route with Switch and import Memory Show

--- src/containers/MemoriesContainer.js
+++ src/containers/MemoriesContainer.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Route, Switch } from 'react-router-dom'
 import MemoryInput from '../components/MemoryInput'
+import MemoryShow from '../components/MemoryShow'
 import Memories from '../components/Memories'
 
 class MemoriesContainer extends React.Component {
@@ -8,6 +11,7 @@ class MemoriesContainer extends React.Component {
         return (
             <div>
                 <MemoryInput /><br/>
+                <MemoryShow memory={this.props.pet && this.props.pet.memory_id}/>
                 <Memories memories={this.props.pet && this.props.pet.memories}/>
             </div>
         )
@@ -15,3 +19,9 @@ class MemoriesContainer extends React.Component {
 }
 
 export default MemoriesContainer;
+
+// <Switch>
+// <Route path='/pets/new' component={PetInput}/>
+// <Route path='/pets/:id' render={(routerProps) => <PetShow { ...routerProps } pets={this.props.pets} /> }/>
+// <Route path='/pets' render={(routerProps) => <Pets { ...routerProps } pets={this.props.pets} /> }/>
+// </Switch>
