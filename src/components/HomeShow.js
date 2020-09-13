6f8b3ac477cd8435045566519fb181e8c66bc4bb
leighad <cdversiontwo@gmail.com>
2020-09-13 15:04:24 -0700

    create Home Show

---
+++ src/components/HomeShow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomeShow = () => {
+
+        return (
+            <div className='centered'>
+                <h3>~ this place was created to honor all animals who have made our lives better ~</h3>
+            </div>
+        )
+}
+
+export default HomeShow;
