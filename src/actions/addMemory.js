586cc6467109ea26a726ee6696d59c1e819cb1af
leighad <cdversiontwo@gmail.com>
2020-09-13 15:02:19 -0700

    add error message

--- src/actions/addMemory.js
+++ src/actions/addMemory.js
@@ -1,15 +1,38 @@
+// export const addMemory = (memory, petId) => {
+
+//     return (dispatch) => {
+//         fetch(`http://localhost:3000/api/v1/pets/${petId}/memories`, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(memory)
+//         })
+//         .then(res => res.json())
+//         .then(pet => dispatch({ type: 'ADD_MEMORY', payload: pet }))
+
+//     }
+// }
+
+
 export const addMemory = (memory, petId) => {
 
     return (dispatch) => {
         fetch(`http://localhost:3000/api/v1/pets/${petId}/memories`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(memory)
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(memory)
         })
         .then(res => res.json())
-        .then(pet => dispatch({ type: 'ADD_MEMORY', payload: pet }))
-
+        .then(pet => {
+            if (pet.error) {
+                alert(pet.error)
+            } else {
+                dispatch({type: 'ADD_MEMORY', payload: pet})
+            }
+        }
+        )
     }
 }
