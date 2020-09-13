36cc732d50ccf2c865ad282832804856d2b4fc1c
leighad <cdversiontwo@gmail.com>
2020-09-13 15:03:48 -0700

    add conditional to map

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -3,11 +3,10 @@ import { Link } from 'react-router-dom'
 // import PetShow from './PetShow'
 
 const Pets = (props) => {
-
     return (
         <div>
             <h2>My Pets: </h2>
-            {props.pets.map(pet => 
+            {props.pets && props.pets.map(pet => 
                 <li key={pet.id}>
                     <Link to={`/pets/${pet.id}`}>{pet.name}</Link>
                 </li> )}
