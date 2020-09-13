ee692ad86232ad306f4b2b13604219fd3397b14f
leighad <cdversiontwo@gmail.com>
2020-09-13 16:38:52 -0700

    render pet details

--- src/components/PetDetails.js
+++ src/components/PetDetails.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 const PetDetails = (props) => {
 
-    console.log(props)
-    // let currentPet = 
+    console.log('pet detail props are', props)
+    console.log('props.pet is', props.pet)
 
     return (
         <div>
-            Pet Details Go Here
-            {/* <h4>`${currentPet.name} the ${currentPet.kind}`</h4> */}
-            
+            { props.pet && props.pet.born ? `was born on ${props.pet.born}` : null }<br/>
+            { props.pet && props.pet.passed ? `crossed the rainbow bridge ${props.pet.passed}` : null }<br/>
+            { props.pet && props.pet.age ? `was ${props.pet.age}` : null }<br/>
+            { props.pet && props.pet.bio ? `${props.pet.bio}` : null }<br/>
         </div>
     )
 }
