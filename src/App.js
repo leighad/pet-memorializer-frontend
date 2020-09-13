4e91f7c78ab4379931040ff864a9c04f903ab7c2
leighad <cdversiontwo@gmail.com>
2020-09-13 15:07:54 -0700

    import components and add Route and NavLink

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { Route, NavLink } from 'react-router-dom'
+import HomeShow from './components/HomeShow'
 import PetsContainer from './containers/PetsContainer'
+import Pets from './components/Pets'
+import PetInput from './components/PetInput'
+import MemoriesContainer from './containers/MemoriesContainer';
 
 // import './App.css';
 
 class App extends React.Component {
 
-  // componentDidMount() {
-  //   this.props.fetchPets()
-  // }
-
   render() {
     return (
       <div className="App">
-        {/* App Component  */}
         <div className="App-Header">
           <h1>Pet Memorializer</h1>
           <h2>a place to chronicle special memories of your pets</h2><hr/>
         </div>
+        <HomeShow />
+        {/* <div className='centered'>
+          <h3>~ this place was created to honor all animals who have made our lives better ~</h3>
+        </div> */}
+        <div>
+          <div className='link-menu'>
+            <NavLink exact activeClassName='active' to='/'>Home</NavLink><br/><br/>
+            <NavLink activeClassName='active' to='/pets'>My Pets</NavLink><br/><br/>
+            <NavLink activeClassName='active' to='/pets/new'>Create A Pet</NavLink>
+          </div>
+          {/* <Route path='/' component={HomeShow} /> */}
+          {/* <Route path='/pets' component={Pets} /> */}
+          {/* <Route path='/pets/new' component={PetInput} /> */}
+        </div>
+
         <PetsContainer />
+        {/* <MemoriesContainer /> */}
       </div>
     );
   }
