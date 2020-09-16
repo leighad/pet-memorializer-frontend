161289569f4530915058744aca4adf1b0fc4ebb8
leighad <cdversiontwo@gmail.com>
2020-09-16 14:27:35 -0700

    add image and create component

---
+++ src/components/TankieShow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TankieShow = () => {
+    return (
+        <img src={require("../assets/Tankie at the Beach.jpg")} alt="Beautiful English Bulldog profile at the beach" width="450" height="600"></img>
+    )
+}
+
+export default TankieShow;
+
