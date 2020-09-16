4be2b7b20d58c13dc71383abdecd14123f8fbd6e
leighad <cdversiontwo@gmail.com>
2020-09-16 14:24:30 -0700

    change div styling and add comment

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Pets = (props) => {
     return (
-        <div>
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+        }}>
             <h2>My Pets: </h2>
-            {props.pets && props.pets.map(pet => 
+            {/* {props.pets && props.pets.map(pet =>  */}
+            {props.pets.map(pet => 
                 <li key={pet.id}>
                     <Link to={`/pets/${pet.id}`}>{pet.name}</Link>
                 </li> )}
