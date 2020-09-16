5e4496807d33ef5fb708f9410410740a40d2fe4d
leighad <cdversiontwo@gmail.com>
2020-09-16 14:17:51 -0700

    add cloud image and component

---
+++ src/components/CloudShow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CloudShow = () => {
+    return (
+        <div>
+            <img src={require("../assets/clouds-spectrum-1278x968.jpg")} alt="Beautiful clouds along the spectrum" width="400" height="600"></img>
+        </div>
+    )
+}
+
+export default CloudShow;
