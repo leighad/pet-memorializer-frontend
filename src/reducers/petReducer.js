4bb3fb0794b7a8ba293aafe6eec3184cc399a0ec
leighad <cdversiontwo@gmail.com>
2020-09-18 11:39:33 -0700

    change action from string to const in Reducer

--- src/reducers/petReducer.js
+++ src/reducers/petReducer.js
@@ -1,13 +1,17 @@
 export default function petReducer(state = {pets: []}, action) {
 
+    const FETCH_PETS = 'FETCH_PETS';
+    const ADD_PET = 'ADD_PET';
+    const ADD_MEMORY = 'ADD_MEMORY';
+
     switch (action.type) {
-        case 'FETCH_PETS':
+        case FETCH_PETS:
             return { pets: action.payload } 
             
-        case 'ADD_PET':
+        case ADD_PET:
             return { ...state, pets: [...state.pets, action.payload]}
 
-        case 'ADD_MEMORY':
+        case ADD_MEMORY:
             let pets = state.pets.map(pet => {
                 if (pet.id === action.payload.id) {
                     return action.payload
